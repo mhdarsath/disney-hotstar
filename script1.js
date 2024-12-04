160108895a65980e3fdff6b67fdf7f47edfa8aac
mhdarsath <arsathmohamed297@gmail.com>
2024-12-04 22:24:54 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+document.getElementById("profile-link").addEventListener("click", function() {
+    const profileMenu = document.getElementById("profile-menu");
+    profileMenu.classList.toggle("show");
+});
+
+// This assumes that you will add a "show" class in your CSS to display the profile menu when active.
